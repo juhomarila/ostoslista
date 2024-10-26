4e7053e4ee01a2429ccd59010e9fcf7158a484c6
Juho Marila <juho.marila@student.lab.fi>
2024-10-26 21:59:13 +0300

    testing pipeline

--- src/main/java/fi/ruoka/ostoslista/controller/OstosListaController.java
+++ src/main/java/fi/ruoka/ostoslista/controller/OstosListaController.java
@@ -5,18 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import fi.ruoka.ostoslista.service.OstosListaService;
-import fi.ruoka.ostoslista.service.OstosService;
 import fi.ruoka.ostoslista.dto.OstosDto;
 import fi.ruoka.ostoslista.dto.OstosListaDto;
 import fi.ruoka.ostoslista.logging.OstosListaLogger;
+import fi.ruoka.ostoslista.service.OstosListaService;
+import fi.ruoka.ostoslista.service.OstosService;
 
 @RestController
 @RequestMapping("/api/ostoslista")
@@ -49,6 +49,7 @@ public class OstosListaController {
         @GetMapping
         public ResponseEntity<?> getAllOstosLista() {
                 logger.getLogStart("getAllOstosLista");
+                logger.postLogStart("Jeejeevittujeejee");
                 var vsr = ostosListaService.getAllOstosLista();
                 logger.getLogEnd("getAllOstosLista");
                 return vsr.getT().isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(vsr.getT());
