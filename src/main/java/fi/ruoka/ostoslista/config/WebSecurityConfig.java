f6a3b4801f5dd017a6ae512c45d30cb4ebb8a6a2
Juho Marila <juho.marila@gmail.com>
2025-01-22 18:30:07 +0200

    preflight cors status added

--- src/main/java/fi/ruoka/ostoslista/config/WebSecurityConfig.java
+++ src/main/java/fi/ruoka/ostoslista/config/WebSecurityConfig.java
@@ -1,23 +1,20 @@
 package fi.ruoka.ostoslista.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvc
 public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -29,25 +26,16 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/user/**").permitAll()
-                .requestMatchers("/api/tuotteet/**").authenticated()
-                .requestMatchers("/api/reseptit/**").authenticated()
-                .requestMatchers("/api/ostoslista/**").authenticated()
-                .anyRequest().authenticated())
+                        .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll() // CORS preflight requests
+                        .requestMatchers("/api/user/**").permitAll()
+                        .requestMatchers("/api/tuotteet/**").authenticated()
+                        .requestMatchers("/api/reseptit/**").authenticated()
+                        .requestMatchers("/api/ostoslista/**").authenticated()
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
