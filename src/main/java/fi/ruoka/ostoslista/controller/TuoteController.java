c84afec1ad3acf2202bf170090ef95e6899be585
Juho Marila <juho.marila@gmail.com>
2025-01-22 18:54:16 +0200

    Crossorigin on perseestä

--- src/main/java/fi/ruoka/ostoslista/controller/TuoteController.java
+++ src/main/java/fi/ruoka/ostoslista/controller/TuoteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tuotteet")
+@CrossOrigin
 public class TuoteController {
 
     @Autowired
--- src/main/java/fi/ruoka/ostoslista/controller/UserController.java
+++ src/main/java/fi/ruoka/ostoslista/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import fi.ruoka.ostoslista.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
